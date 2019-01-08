8579553733a5486476d33282f7998f89fabfd60a
rachel.xiao <rachel.xiao@wolaidai.com>
2019-01-08 17:04:48 +0800

    增加提交评论方法

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -12,14 +12,24 @@ const Comments = React.createClass({
       </div>
     )
   },
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { postId } = this.props.params;
+    const author = this.refs.author.value;
+    const comment = this.refs.comment.value;
+    this.props.addComment(postId, author, comment);
+    this.refs.commentForm.reset();
+  },
+
   render() {
     return (
       <div className="comments">
         {this.props.postComments.map(this.renderComment)}
-        <form ref="commentForm" className="comment-form">
+        <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
           <input type="text" ref="author" placeholder="author"/>
           <input type="text" ref="comment" placeholder="comment"/>
-          <input type="submit" hidden />
+          <input type="submit" />
         </form>
       </div>
     )
--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,4 +1,4 @@
-function comments(state = [], action) {
+function postComments(state = [], action) {
   switch(action.type) {
     case 'ADD_COMMENT':
       // return the new state with the new comment
@@ -23,7 +23,9 @@ function comments(state = [], action) {
 function comments(state = [], action) {
   if (typeof action.postId !== 'undefined') {
     return {
+      // take the current state
       ...state,
+      // overwrite this post with a new one
       [action.postId]: postComments(state[action.postId], action)
     }
   }
